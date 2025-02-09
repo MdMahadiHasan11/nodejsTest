09ae512334209a5caedaa32d00fdb02a7a8eff62
MdMahadiHasan11 <hasanpustcse11@gmail.com>
2025-02-09 23:12:52 +0600

    html file added

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const http = require("http")
 const fs = require("fs")
+const express = require("express")
+
+const app = express()
 const PORT = 3000;
 // const PORT = process.env.PORT
 
-const myServer = http.createServer((req, res)=> {
-   res.end("hello")
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
 })
 
-myServer.listen(PORT,()=>{
+app.use(express.static("public"))
+
+// const myServer = http.createServer((req, res)=> {
+//    res.end("hello")
+// })
+
+app.listen(PORT,()=>{
     console.log(`server running at ${PORT}`);
 })
